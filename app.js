549f259df73230ebb16ede58396abc9aec8a668a
Jason Howie <howiejason93@gmail.com>
2019-07-27 11:57:32 -0400

    add middleware

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
+// Requires the necessary dependancies and files
 const express = require('express');
-// const data = require('./public/images/');
+const data = require('./data.json');
 
+// Creates the app and sets the port
 const app = express();
 const port = 3000;
 
+// Sets up necessary middleware
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-    res.send('It Works!');
+    res.render('index', {data: data.projects});
 });
 
+// Listens on the set port
 app.listen(port, () => {
     console.log(`Listening on port ${port}.`);
 });
